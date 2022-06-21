c8b860e656b70bd7c546db77187b475a5d19fe8a
David Henry <dah06a@acu.edu>
2022-06-20 20:30:52 -0500

    Removed boundary block visibility

--- code/level.py
+++ code/level.py
@@ -23,7 +23,7 @@ class Level:
                         x = col_index * TILESIZE
                         y = row_index * TILESIZE
                         if style == 'boundary':
-                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'invisible')
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
         #         if col == 'x':
         #             Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
         #         if col == 'p':
