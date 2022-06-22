b8568bc34039f76cfc04f2d522a81798552bcbf7
David Henry <dah06a@acu.edu>
2022-06-22 08:59:39 -0500

    Function complete for keeping track of idle, moving, and attack status.

--- code/player.py
+++ code/player.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
@@ -8,6 +9,10 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -25)
+
+        # Player graphics setup
+        self.import_player_assets()
+        self.status = 'down'
 
         # Setup movement
         self.direction = pygame.math.Vector2()
@@ -18,6 +23,18 @@ class Player(pygame.sprite.Sprite):
 
         self.obstacle_sprites = obstacle_sprites
     
+    def import_player_assets(self):
+        character_path = '../graphics/player/'
+        self.animations = {
+            'up': [], 'down': [], 'left': [], 'right': [],
+            'up_idle': [], 'down_idle': [], 'left_idle': [], 'right_idle': [],
+            'up_attack': [], 'down_attack': [], 'left_attack': [], 'right_attack': []
+        }
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
     def input(self):
         # Get list of any keys pressed down during frame
         keys = pygame.key.get_pressed()
@@ -25,15 +42,19 @@ class Player(pygame.sprite.Sprite):
         # Movement input - up, down, left, right
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
         if keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         else:
             self.direction.x = 0
 
@@ -48,6 +69,25 @@ class Player(pygame.sprite.Sprite):
             self.attacking = True
             self.attack_time = pygame.time.get_ticks()
             print('magic')
+
+    def get_status(self):
+        # Check idle status
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not 'idle' in self.status and not 'attack' in self.status:
+                self.status = self.status + '_idle'
+
+        # Check attacking status
+        if self.attacking:
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status:
+                if 'idle' in self.status:
+                    self.status = self.status.replace('_idle', '_attack')
+                else:
+                    self.status = self.status + '_attack'
+        else:
+            if 'attack' in self.status:
+                self.status = self.status.replace('_attack', '')
 
     def move(self, speed):
         # Adjust speed for diagonal screen movement
@@ -86,8 +126,8 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
 
-             
     def update(self):
         self.input()
         self.cooldowns()
+        self.get_status()
         self.move(self.speed)
